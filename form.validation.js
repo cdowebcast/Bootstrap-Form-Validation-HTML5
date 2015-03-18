19deb5268127347f0113b67d6e5994334dda7662
jodacame <jodacame@gmail.com>
2015-03-18 18:36:02 -0500

    Version 0.1

---
+++ form.validation.js
@@ -0,0 +1,27 @@
+jQuery.fn.html5validate = function() { 	
+	var form = $(this);	
+	var error_div = $("<div class='alert alert-danger'></div>");
+	var error_ul = $("<ul></ul>");
+	error_div.append(error_ul);
+	form.prepend(error_div.hide());	
+	console.log("a");
+	form.on('click',':submit', function(event) {			
+			error_div.hide();
+			error_ul.empty();
+			if(!form[0].checkValidity())			
+			{							
+			
+				form.find( ":invalid" ).each( function( index, node ) {				
+					message 	= node.validationMessage || 'Invalid value.';			
+					label 		= $(this).attr("title")  || 'Error';			
+					var el 		= $("<li>"+label+": "+message+"</li>");
+					error_div.show();
+					error_ul.append(el);				
+					event.preventDefault();
+				});	
+			}			
+
+			
+	});	
+  
+};
